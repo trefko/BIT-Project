172409512e36b314c996a7ba7e4a1b540904ce7b
trefko <stefi.milovanovic99@gmail.com>
2024-11-18 15:28:58 +0100

    Gotov BIT Movie Projekat

---
+++ js/main.js
@@ -0,0 +1,65 @@
+const URL = "https://api.tvmaze.com/shows";
+const container = document.querySelector(".container");
+
+function getMovies() {
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+      showMovies(data);
+    });
+}
+
+function showMovies(data) {
+  console.log(data);
+  const newMoviesArray = data.slice(0, 51);
+
+  newMoviesArray.forEach((e) => {
+    const moviesDiv = document.createElement("div");
+    const moviesPic = document.createElement("img");
+    const moviesText = document.createElement("p");
+
+    // Check if image exists before setting src
+    if (e.image) {
+      moviesPic.setAttribute("src", e.image.medium);
+    }
+    moviesText.innerHTML = e.name;
+    moviesDiv.append(moviesPic, moviesText);
+    container.append(moviesDiv);
+
+    moviesDiv.addEventListener("click", () => {
+      console.log(e);
+      localStorage.setItem("show", JSON.stringify(e));
+      location.href = "../BitShow/show-info.html";
+    });
+  });
+}
+const result = document.querySelector(".result");
+function getOption(string) {
+  const API = `https://api.tvmaze.com/search/shows?q=${string}`;
+  fetch(API)
+    .then((res) => res.json())
+    .then((data) => {
+      makeOptions(data);
+    });
+}
+
+function makeOptions(data) {
+  result.innerHTML = "";
+
+  data.forEach((e) => {
+    const txt = document.createElement("p");
+    txt.textContent = e.show.name;
+    result.append(txt);
+    txt.addEventListener("click", () => {
+      localStorage.setItem("show", JSON.stringify(e.show));
+      location.href = "../BitShow/show-info.html";
+    });
+  });
+}
+
+const search = document.querySelector(".search");
+search.addEventListener("keyup", () => {
+  getOption(search.value);
+});
+
+window.addEventListener("load", getMovies);
---
+++ js/show-info.js
@@ -0,0 +1,62 @@
+const back = document.querySelector(".head>h3");
+
+function getShow(card) {
+  const img = document.querySelector(".img");
+  img.src = card.image.original;
+
+  const id = document.querySelector(".id");
+  id.textContent = `Id: ${card.id}`;
+
+  const show = document.querySelector(".show-name");
+  show.textContent = `Name: ${card.name}`;
+
+  const status = document.querySelector(".status");
+  status.textContent = `Status: ${card.status}`;
+
+  const runtime = document.querySelector(".runtime");
+  runtime.textContent = `Runtime: ${card.runtime}`;
+
+  const premiered = document.querySelector(".premiered");
+  premiered.textContent = `Premiered: ${card.premiered}`;
+
+  const description = document.querySelector(".description");
+  description.innerHTML = `Description: ${card.summary}`;
+}
+
+const result = document.querySelector(".result");
+function getOption(string) {
+  const API = `https://api.tvmaze.com/search/shows?q=${string}`;
+  fetch(API)
+    .then((res) => res.json())
+    .then((data) => {
+      makeOptions(data);
+    });
+}
+
+function makeOptions(data) {
+  result.innerHTML = "";
+
+  data.forEach((e) => {
+    const txt = document.createElement("p");
+    txt.textContent = e.show.name;
+    result.append(txt);
+    txt.addEventListener("click", () => {
+      localStorage.setItem("show", JSON.stringify(e.show));
+      location.href = "../BitShow/show-info.html";
+    });
+  });
+}
+
+back.addEventListener("click", () => {
+  window.location.pathname = "../BitShow/index.html";
+});
+
+const search = document.querySelector(".search");
+search.addEventListener("keyup", () => {
+  getOption(search.value);
+});
+
+window.addEventListener("load", () => {
+  const getInfo = JSON.parse(localStorage.getItem("show"));
+  getShow(getInfo);
+});
