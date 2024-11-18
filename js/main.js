42ad916871f301344b263ac0e5b3fc5226327b88
trefko <stefi.milovanovic99@gmail.com>
2024-11-18 17:29:25 +0100

    Dodat debounce

--- js/main.js
+++ js/main.js
@@ -58,8 +58,21 @@ function makeOptions(data) {
 }
 
 const search = document.querySelector(".search");
-search.addEventListener("keyup", () => {
-  getOption(search.value);
-});
+search.addEventListener(
+  "keyup",
+  debounce(() => {
+    getOption(search.value);
+  })
+);
+
+function debounce(func, timeout = 700) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
 
 window.addEventListener("load", getMovies);
